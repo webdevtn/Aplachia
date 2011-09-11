d657f7eb70369f1fcd010944de2630ecfd83de94
unknown <Bert@.(none)>
2011-09-11 09:45:22 -0400

    Initial Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aplachia::Application.config.secret_token = '922368ba9c6c17e052cd9096a81322bb0e7d4fdf38a0c44704a97ca579d3b08750e2893db86cd1f7425e04cf785379fe8cc3ddd7f7e75ebc6cfa947eadc0743a'
